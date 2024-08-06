6a4ea3b7b9ec9b0cdad86a6d65c2f3b481d15f46
vigiaquinze <victorhugopagliuca84@gmail.com>
2024-08-06 14:36:27 -0300

    06/08 14h35

---
+++ Programacao4oSemestre/PHP Projects/GerenciamentoProduto/app/Http/Controllers/ProdutoController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Produto;
+use Illuminate\Http\Request;
+
+class ProdutoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $produtos = Produto::all();
+        return view('produtos.index', compact('produtos'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('produtos.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nome' => 'required',
+            'preco' => 'required|numeric',
+        ]);
+
+        Produto::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Produto $produto)
+    {
+        return view('produtos.show', compact('produto'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Produto $produto)
+    {
+        return view('produto.edit', compact('produto'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Produto $produto)
+    {
+        $request->validate([
+            'nome' => 'required',
+            'preco' => 'required|decimal'
+        ]);
+
+        $produto->update($request->all());
+        return redirect()->route('produtos.index')
+            ->with('success', 'Produto atualizado com sucesso.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Produto $produto)
+    {
+        $produto->delete();
+        return redirect()->route('produtos.index')
+            ->with('success', 'Produto removido com sucesso.');
+    }
+}
